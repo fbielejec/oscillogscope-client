fd005759e1a954effad24507f40babf70f18300e
filip <fbielejec@gmail.com>
2016-05-24 16:02:27 +0200

    styles for reste button

--- app/containers/Home/HomeContainer.js
+++ app/containers/Home/HomeContainer.js
@@ -17,9 +17,26 @@ handleWatch(event) {
     return;
   }
 
-console.log(file);
+  var reader = new FileReader();
+  reader.readAsText(file, 'UTF-8');
+  var self = this;
+
+  reader.onload = function(e) {
+      var content = e.target.result;
+      // Entire file
+      // console.log(content);
+
+      // By lines
+      var lines = content.split('\n');
+  // console.log(lines[2]);
+      for (var line = 0; line < lines.length; line++) {
+        console.log(lines[line]);
+      }
+
+// TODO: read and send column names server-side (server created db)
 
 
+    } //END: onLoad
 },
 
   render() {
